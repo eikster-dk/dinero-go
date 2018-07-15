db7ff4f6f3c14bbb2556011c6878a1e5497c9bff
Eik Emil Christensen <me@eikc.dk>
2018-07-15 18:06:14 +0200

    added package to handle account endpoints

---
+++ accounts/accounts.go
@@ -0,0 +1,59 @@
+package accounts
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/eikc/dinero-go"
+)
+
+// Deposit represents a deposit in dinero regnskab
+type Deposit struct {
+	Number int    `json:"AccountName"`
+	Name   string `json:"Name"`
+}
+
+// Params for filtering what is returned for both deposits and accounts
+const (
+	Name   = "Name"
+	Number = "AccountNumber"
+)
+
+// Account represents an account in dinero regnskab
+type Account struct {
+	Number int    `json:"AccountName"`
+	Name   string `json:"Name"`
+}
+
+// GetAccounts Gets the list of entry accounts for the organization
+func GetAccounts(api dinero.API, wantedFields ...string) ([]Account, error) {
+	route := getRouteWithFields("v1/{organizationID}/accounts/entry", wantedFields)
+
+	var accounts []Account
+	if err := api.Call(http.MethodGet, route, nil, &accounts); err != nil {
+		return nil, err
+	}
+
+	return accounts, nil
+}
+
+// GetDeposits gets the list of deposit accounts for the organization
+func GetDeposits(api dinero.API, wantedFields ...string) ([]Deposit, error) {
+	route := getRouteWithFields("v1/{organizationID}/accounts/deposit", wantedFields)
+
+	var deposits []Deposit
+	if err := api.Call(http.MethodGet, route, nil, &deposits); err != nil {
+		return nil, err
+	}
+
+	return deposits, nil
+}
+
+func getRouteWithFields(route string, wantedFields []string) string {
+	if wantedFields != nil {
+		fieldQuery := dinero.BuildFieldsQuery(wantedFields...)
+		route = fmt.Sprintf("%v?fields=%v", route, fieldQuery)
+	}
+
+	return route
+}
---
+++ accounts/accounts_test.go
@@ -0,0 +1,122 @@
+package accounts
+
+import (
+	"testing"
+
+	"github.com/eikc/dinero-go"
+	"github.com/eikc/dinero-go/dinerotest"
+)
+
+func Test_getRouteWithFields(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		route        string
+		wantedFields []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Test route for accounts with nil fields", args{"v1/{organizationId}/accounts/entry", nil}, "v1/{organizationId}/accounts/entry"},
+		{"test routes for account with name field", args{"v1/{organizationId}/accounts/entry", []string{Name}}, "v1/{organizationId}/accounts/entry?fields=Name"},
+		{"test routes for account with name and number field", args{"v1/{organizationId}/accounts/entry", []string{Name, Number}}, "v1/{organizationId}/accounts/entry?fields=Name,AccountNumber"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getRouteWithFields(tt.args.route, tt.args.wantedFields); got != tt.want {
+				t.Errorf("getRouteWithFields() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetDeposits_integration(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip(dinerotest.IntegrationTestText)
+	}
+
+	key, secret, apiKey, organizationID := dinerotest.GetClientKeysForIntegrationTesting()
+
+	c := dinero.NewClient(key, secret)
+	c.Authorize(apiKey, organizationID)
+
+	_, err := GetDeposits(c)
+	if err != nil {
+		t.Errorf("got an error while calling the deposits endpoint: %v", err)
+	}
+}
+
+func TestGetDeposits_OnlyReturnNumberField_integration(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip(dinerotest.IntegrationTestText)
+	}
+
+	key, secret, apiKey, organizationID := dinerotest.GetClientKeysForIntegrationTesting()
+
+	c := dinero.NewClient(key, secret)
+	c.Authorize(apiKey, organizationID)
+
+	deposits, err := GetDeposits(c, Number)
+	if err != nil {
+		t.Errorf("Got an error whil calling the deposits endpoint %v", err)
+	}
+
+	first := deposits[0]
+
+	var defaultString string
+	var defaultInt int
+	if first.Name != defaultString && first.Number == defaultInt {
+		t.Errorf("the returned accounts are including the Name, first is: %v", first)
+	}
+}
+
+func TestGetAccounts_integration(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip(dinerotest.IntegrationTestText)
+	}
+
+	key, secret, apiKey, organizationID := dinerotest.GetClientKeysForIntegrationTesting()
+
+	c := dinero.NewClient(key, secret)
+	c.Authorize(apiKey, organizationID)
+
+	_, err := GetAccounts(c)
+	if err != nil {
+		t.Errorf("Got an error while fetching accounts: %v", err)
+	}
+}
+
+func TestGetAccountsWithOnlyName_Integration(t *testing.T) {
+	t.Parallel()
+
+	if testing.Short() {
+		t.Skip(dinerotest.IntegrationTestText)
+	}
+
+	key, secret, apiKey, organizationID := dinerotest.GetClientKeysForIntegrationTesting()
+
+	c := dinero.NewClient(key, secret)
+	c.Authorize(apiKey, organizationID)
+
+	accounts, err := GetAccounts(c, Name)
+	if err != nil {
+		t.Errorf("Got an error while fetching accounts with name field only: %v", err)
+	}
+
+	first := accounts[0]
+
+	var defaultString string
+	var defaultNumber int
+
+	if first.Name == defaultString && first.Number != defaultNumber {
+		t.Errorf("The returned account is not what is expected: %v", first)
+	}
+}
