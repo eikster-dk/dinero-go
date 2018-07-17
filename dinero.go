715c13aa03a354ec8a2f8123151cad8eebbbf171
Eik Emil Christensen <me@eikc.dk>
2018-07-17 19:52:06 +0200

    added dinero.Time to parse the json formatted timestamp from dinero correctly

--- dinero.go
+++ dinero.go
@@ -11,6 +11,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
@@ -51,6 +52,37 @@ type PaginationResult struct {
 	Result              int
 	ResultWithoutFilter int
 	Page                int
+}
+
+// Time is a wrapper for time to make sure the
+// JSON returned from dinero is correctly parsed
+type Time struct {
+	time.Time
+}
+
+const dineroLayout = "2006-01-02T15:04:05.000"
+
+// UnmarshalJSON is Helper function to parse the Timestamp from dinero
+func (dt *Time) UnmarshalJSON(b []byte) error {
+	s := strings.Trim(string(b), "\"")
+	if s == "null" {
+		dt.Time = time.Time{}
+		return nil
+	}
+
+	t, err := time.Parse(dineroLayout, s)
+	if err != nil {
+		return err
+	}
+
+	dt.Time = t
+
+	return nil
+}
+
+// MarshalJSON is Helper function to format a dinero timestamp
+func (dt *Time) MarshalJSON() ([]byte, error) {
+	return []byte(dt.Time.Format(dineroLayout)), nil
 }
 
 // Authorize will authorize the client
