a7016cc9c1725b08b2cfc871f5b99d9fa35ffeaa
Eik Emil Christensen <me@eikc.dk>
2018-07-20 19:51:09 +0200

    extracted payment condition into dinero package

--- paymentConditions.go
+++ paymentConditions.go
@@ -6,3 +6,9 @@ const (
 	NettoCash       = "NettoCash"
 	CurrentMonthOut = "CurrentMonthNettoOut"
 )
+
+// PaymentConditions represents the properties for paymentConditions
+type PaymentConditions struct {
+	PaymentConditionNumberOfDays int    `json:"paymentConditionNumberOfDays,omitempty"`
+	PaymentConditionType         string `json:"paymentConditionType,omitempty"`
+}
